838caaec88818c04c875dea4654fc60ee135d0ba
Labrock <vladimirpeshkov5@yandex.ru>
2025-03-25 16:30:17 +0300

    add new trait

--- src/Models/SimpleBlockRecord.php
+++ src/Models/SimpleBlockRecord.php
@@ -5,21 +5,15 @@ namespace GIS\EditableBlocks\Models;
 use GIS\EditableBlocks\Interfaces\SimpleBlockRecordModelInterface;
 use GIS\EditableBlocks\Traits\ShouldBlockItem;
 use GIS\Fileable\Traits\ShouldImage;
+use GIS\TraitsHelpers\Traits\ShouldMarkdown;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class SimpleBlockRecord extends Model implements SimpleBlockRecordModelInterface
 {
-    use ShouldBlockItem, ShouldImage;
+    use ShouldBlockItem, ShouldImage, ShouldMarkdown;
 
     protected $fillable = [
         "description",
     ];
-
-    public function getMarkdownAttribute(): ?string
-    {
-        $value = $this->description;
-        if (! $value) return $value;
-        return Str::markdown($value);
-    }
 }
